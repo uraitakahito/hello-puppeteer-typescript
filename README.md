bb5312df24a0c74680aa65077c1687d68b350589
Takahito Urai <uraitakahito@gmail.com>
2025-11-27 18:46:00 +0900

    Add type annotation and Puppeteer launch args, update .gitignore, add README

---
+++ README.md
@@ -0,0 +1,27 @@
+## Setup
+
+Please download the required files by following these steps:
+
+```
+curl -L -O https://raw.githubusercontent.com/uraitakahito/hello-puppeteer/refs/heads/main/Dockerfile
+curl -L -O https://raw.githubusercontent.com/uraitakahito/hello-javascript/refs/heads/main/docker-entrypoint.sh
+chmod 755 docker-entrypoint.sh
+```
+
+Detailed environment setup instructions are described at the beginning of the `Dockerfile`.
+
+The noVNC can be accessed at:
+
+- http://localhost:6080/
+
+Run the following commands inside the Docker containers:
+
+```sh
+node examples/get-title.mjs
+```
+
+You can check if you have enough memory by running this command:
+
+```console
+% numfmt --to iec $(echo $(($(getconf _PHYS_PAGES) * $(getconf PAGE_SIZE))))
+```
--- src/title.ts
+++ src/title.ts
@@ -1,10 +1,24 @@
 import puppeteer from "puppeteer";
 
-const getTitle = async (url, headless = true) => {
+const getTitle = async (url: string, headless = true) => {
   // Launch a new browser instance
   const browser = await puppeteer.launch({
     headless,
-  });
+    args: [
+      // Disables GPU hardware acceleration. If software renderer
+      // is not in place, then the GPU process won't launch.
+      //
+      // AND
+      // WORKAROUND:
+      //   - https://stackoverflow.com/questions/66402124/puppeteer-blocked-at-newpage
+      '--disable-gpu',
+      // The sandbox requires kernel features (namespaces) that are typically
+      // unavailable in containers. See: https://pptr.dev/troubleshooting
+      '--no-sandbox',
+      // Companion flag for --no-sandbox when running as non-root.
+      '--disable-setuid-sandbox',
+    ],
+});
 
   // Open a new page
   const page = await browser.newPage();
